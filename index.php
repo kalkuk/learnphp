4aff760c4f0c87b928fa6611f6414e566f9fc1e3
Merle Jõgisman <kalku@lap.ee>
2023-09-22 17:34:24 +0300

    add simple string and number examples

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$num = 1;
+echo($num);
+echo 'hello world';
+var_dump($num);
+var_dump($num);
+$num = 1.4;
+var_dump($num);
+$num ++;
+$num /=2;
+$num =5;
+$num %=2;
+var_dump($num);
+$string = 'sõne';
+$string = 'hello ' .$string;
+$string .= '!!!';
+var_dump($string);
+$hello = "word is $string";
