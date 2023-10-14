223822e6c7cef7bbc7b0258acdada9e9052e1c45
Merle Jõgisman <kalku@lap.ee>
2023-10-14 12:41:49 +0300

    static properties

--- index.php
+++ index.php
@@ -1,26 +1,17 @@
 <?php
-class Job {
-    public function work (ConsoleLogger$logger){
-        for ($i=0; $i<10; $i++){
-            $logger ->log($i);
-            
-    }
-}
-class ConsoleLogger {
-    public function log($text){
-        echo $text . "\n";
-    }
-}
-interface Logger {
-    public function log($text);
-}
-//kasutaja kood
-class NothingLogger implements Logger{
-    public function log($text){
-
+class Box{
+    public $width;
+    public static $count =0;
+    pubblic static funstion getCount(){
+        var_dump(Box::$count);    //self::$count - self viitab endale
+        //staatilise funktsiooni puhul ei saa kasutada $this
+        //
     }
 }
 
-$job = new Job();
-$logger = new NothingLogger
-$job->work('asdasd');
+$box1 = new Box();
+$box2 = new Box();
+$box1::$count =1;
+$box2::$count2 =2;
+var_dump($box1::$count);
+var_dump($box2::$count2);
