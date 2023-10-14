0944a94f573c6c09a85b9ce6df8046c8fd3368d9
Merle Jõgisman <kalku@lap.ee>
2023-10-14 13:09:36 +0300

    i

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php
 class Box{
-    public $width;
-    public static $count =0;
-    pubblic static funstion getCount(){
-        var_dump(Box::$count);    //self::$count - self viitab endale
-        //staatilise funktsiooni puhul ei saa kasutada $this
-        //
+    public function __construct($width, $height){
+        var_dump('construct', $width, $height);
     }
-}
-
-$box1 = new Box();
-$box2 = new Box();
-$box1::$count =1;
-$box2::$count2 =2;
-var_dump($box1::$count);
-var_dump($box2::$count2);
+  }
+  $box = new Box();
+  var_dump('after $box');
